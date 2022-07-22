12484de357325eb1e9a1970f0f50d32b692609ff
ngotokodawari <naoya.goto@kodawari-japan.co.jp>
2022-07-22 14:38:53 +0900

    App.jsからComponetCに値を渡す

--- App.tsx
+++ App.tsx
@@ -1,14 +1,26 @@
 import * as React from 'react';
 import './style.css';
+import { createContext } from 'react';
 import ComponentA from './components/ComponentA';
+export const UserCount = createContext();
+
 export default function App() {
   return (
     <div style={{ textAlign: 'center' }}>
       <h1>React Hooksで子から親へデータを共有するサンプル</h1>
-      <a href="https://reffect.co.jp/react/react-usecontext-understanding">
-        参考元
-      </a>
-      <ComponentA />
+      <ul>
+        <a href="https://reffect.co.jp/react/react-usecontext-understanding">
+          参考元
+        </a>
+      </ul>
+      App.jsから数字100をContext APIを利用してComponetCに渡す。
+      <ul>1.App.js</ul>
+      <ul>2.ComponentA</ul>
+      <ul>3.ComponentB</ul>
+      <ul>4.ComponentC</ul>
+      <UserCount.Provider value={100}>
+        <ComponentA />
+      </UserCount.Provider>
     </div>
   );
 }
--- components/ComponentC.tsx
+++ components/ComponentC.tsx
@@ -1,8 +1,15 @@
 import * as React from 'react';
+import { useContext } from 'react';
+//ComponentCでuseContextを利用するためimportします。
+import { UserCount } from '../App';
+//App.jsでexportしたUserCountはここでimportする必要があります。
 const ComponentC = () => {
+  const count = useContext(UserCount);
+  //useContextとUserCountを使ってvalueで設定した値を取り出し変数countに入れます。
   return (
     <div>
       <p>Componet C</p>
+      <p>App.jsから受け取った値={count}</p>
     </div>
   );
 };
